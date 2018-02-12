bd55652703d36045db9d446cb70374ed4a0c07ac
jasper <realshuqian@yahoo.com>
2018-02-12 21:34:16 +0800

    add properties test

--- src/main/java/com/java/maven/test/hibernatetry/CrUser.java
+++ src/main/java/com/java/maven/test/hibernatetry/CrUser.java
@@ -1,8 +1,37 @@
 package com.java.maven.test.hibernatetry;
-public class CrUser implements java.io.Serializable{
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * <pre>
+ CREATE TABLE cr_user(
+ `cuserid` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+ `usercode` VARCHAR(10) NOT NULL,
+ `username` VARCHAR(20) NOT NULL,
+ PRIMARY KEY (`cuserid`) USING BTREE,
+ </pre>
+ )
+ */
+@Entity
+@Table(name="cr_user")
+public class CrUser implements Serializable {
+    private static final long serialVersionUID = 5345245254235252L;
+    @Id
+    @Column(name = "cuserid")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column(name = "usercode")
     private String code;
+    @Column(name = "username")
     private String name;
+    @Column(name = "createdtime")
+    private String createdTime;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    private Set<House> houses=new HashSet<House>();
     //提供默认的构造方法
     public CrUser(){
 
@@ -24,5 +53,21 @@ public class CrUser implements java.io.Serializable{
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<House> getHouses() {
+        return houses;
+    }
+
+    public void setHouses(Set<House> houses) {
+        this.houses = houses;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
     }
 }
--- src/main/java/com/java/maven/test/hibernatetry/HibernateTest.java
+++ src/main/java/com/java/maven/test/hibernatetry/HibernateTest.java
@@ -1,7 +1,14 @@
 package com.java.maven.test.hibernatetry;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class HibernateTest {
     public static void main( String[] args )
@@ -9,12 +16,46 @@ public class HibernateTest {
         Session session = HibernateUtil.getSession();
         Transaction tx = session.beginTransaction();
         try{
+            //Write
             CrUser crUser = new CrUser();
             crUser.setCode("123");
             crUser.setName("hibernate demo");
-            session.save(crUser);//这里操作的是java对象
+            crUser.setCreatedTime(getCurrentTime());
+            session.saveOrUpdate(crUser);//这里操作的是java对象
+
+            Set<House> myHouses= new HashSet<House>();
+            House myHouse=new House();
+            myHouse.setAddress("Dong bu ling dong");
+            myHouse.setUser(crUser);
+            myHouse.setAera("80");
+            session.saveOrUpdate(myHouse);//save first one
+            myHouses.add(myHouse);
+
+            House myHouse2=new House();
+            myHouse2.setAddress("Po Bo Po Bo dong");
+            myHouse2.setUser(crUser);
+            myHouse2.setAera("90");
+            session.saveOrUpdate(myHouse2);//save second one
+            myHouses.add(myHouse2);
+
+            crUser.setHouses(myHouses);
+            session.saveOrUpdate(crUser);//这里操作的是java对象
             tx.commit();
             System.out.println("保存成功!");
+
+
+            //read
+            Query query =session.createQuery("from CrUser");
+            List<CrUser> list = query.list();
+            //使用forEach遍历集合
+            for (CrUser user : list) {
+                System.out.println(user.getId());
+                System.out.println("code="+user.getCode());
+                System.out.println("name="+user.getName());
+                Set<House> houses = user.getHouses();
+                for(House house:houses)
+                System.out.println("houseId="+house.getId());
+            }
         }catch(Exception e){
             e.printStackTrace();
             tx.rollback();
@@ -22,7 +63,13 @@ public class HibernateTest {
         }finally{
             HibernateUtil.closeSession();
         }
+        System.exit(-1);
 
+    }
 
+    static String getCurrentTime(){
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMddE HH:mm:ss");
+        return sdf.format(d);
     }
 }
